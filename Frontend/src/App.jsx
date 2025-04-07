a0b8f41ac78c9bbd49f186638c058849e8f7a5e8
Suraj-Dev14 <surajgaikwad141103@gmail.com>
2025-04-07 14:02:42 +0530

    First Commit

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "./component/Home";
+import Login from "./component/Login";
+import Register from "./component/Register";
+
+function App() {
+
+  return (
+    <Home />
+);
+}
+
+export default App;
